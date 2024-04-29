f206c251a704f9f57fe9d18b9ece972eda5b599b
muzzled2 <161974542+muzzled2@users.noreply.github.com>
2024-04-28 20:25:25 -0700

    Prefabpart1

---
+++ aleal_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if
+            (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(prefab);
+        }
+        if
+            (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
