eeb4c18b768b99bc92a1932463c32bb06e8834e9
muzzled2 <161974542+muzzled2@users.noreply.github.com>
2024-04-28 21:11:44 -0700

    Lamps Part 2

--- aleal_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ aleal_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,7 +5,19 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public int numberofLamps = 10;
+    public float spacing = 6.0f;
 
+    void Start()
+    {
+        Vector3 startPosition = transform.position - new Vector3(spacing * (numberofLamps - 1) / 6f, 0, 0);
+
+        for (int i =0; i < numberofLamps; i++)
+        {
+            Vector3 position = startPosition + new Vector3(i * spacing, 0, 0);
+            Instantiate(prefab, position, Quaternion.identity);
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,7 +30,7 @@ public class PrefabGenerator : MonoBehaviour
         if
             (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(prefab, transform.position, transform.rotation);
+            Instantiate(prefab, transform.position, Quaternion.identity);
         }
     }
 }
