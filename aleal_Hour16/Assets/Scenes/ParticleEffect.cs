4470710c11d8317fe28fc50d9e2bf2407004753d
muzzled2 <161974542+muzzled2@users.noreply.github.com>
2024-05-04 19:09:55 -0700

    Particle Systems

---
+++ aleal_Hour16/Assets/Scenes/ParticleEffect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffect : MonoBehaviour
+{
+
+    //Spiraling Fiery Embers Particle Effect
+
+    // Changed Emission Rate over time to 100 particles per second
+
+    // Changed particle shapes to spheres with a radius of 2
+
+    //Added Color over life time gradient
+
+    //Added size over life time curve <~0.1 - 2>
+
+    //size by speed range 1-5
+
+    //checked rotation by speed box w/ angular velocity at 45
+
+    //added noise w/ strength at 3
+}
