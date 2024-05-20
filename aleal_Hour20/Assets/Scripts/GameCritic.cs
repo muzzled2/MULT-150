19736a82aed557a243be1cc6233ed1f7a0e3d6ca
muzzled2 <161974542+muzzled2@users.noreply.github.com>
2024-05-19 19:42:25 -0700

    Gauntlet Runner

---
+++ aleal_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCritic : MonoBehaviour
+{
+    //Is this game too easy or hard?
+
+    //I think its too easy as you can avoid the obstacles and grab the power ups with minimal effort. You don't even need to use the phase ability to avoid them.
+    //In fact, using it actually hinders you, by preventing you from grabbing the power ups.
+
+    //What 2 changes would you do to make it easier or harder?
+
+    //I'd increase the speed that obstacles come towards the player and add an additional obstacle type thats unavoidable without using the phase ability.
+
+    //List 2 things that could give it a "wow" factor
+
+    //A power up that increases the Worldspeed further
+    //a looping soundtrack to go with the frenetic nature of the game
+
+    //What 2 parts of the game are fun?
+
+    //Avoiding the obstacles and collecting the power ups is fun. That's pretty much all you can do in the game though.
+
+    //What 2 parts of the game are tedious?
+
+    //The player character strafes a bit too slowly for my taste.
+
+    //There's nothing else to do but collect power ups and dodge obstacles.
+
+
+    //CHANGES
+
+    //obstacle model updated to fit the theme better. slightly more difficult to avoid due to the increased size
+
+    //power up model changed to a clock with plus sign to reflect what it does for the player
+}
---
+++ aleal_Hour20/Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+
+    }
+
+ 
+}
